fbc59055579605da029b7d6f7ebb25e41fbf26d3
defanor <defanor@uberspace.net>
2017-11-28 02:51:23 +0300

    Prepend license notices to source files

--- Pancake.hs
+++ Pancake.hs
@@ -1,11 +1,27 @@
+{-
+Copyright (C) 2017  defanor <defanor@uberspace.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 {- |
 Description :  A CLI web\/gopher browser
 Maintainer  :  defanor <defanor@uberspace.net>
 Stability   :  unstable
 Portability :  non-portable (uses GHC extensions)
 
-A CLI web\/gopher\/file browser inspired by
-<https://en.wikipedia.org/wiki/Line_Mode_Browser Line Mode Browser>.
+A CLI\/Emacs web\/gopher\/file browser inspired by LMB.
 
 -}
 
